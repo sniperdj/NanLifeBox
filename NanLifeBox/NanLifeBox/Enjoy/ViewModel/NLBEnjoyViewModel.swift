8f7d0ce5dcc3d3cf0d1bea02e6a30e7d62cb9bb5
郑楠 <sniper_zheng@163.com>
2018-08-26 15:12:07 +0800

    rac

---
+++ NanLifeBox/NanLifeBox/Enjoy/ViewModel/NLBEnjoyViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  NLBEnjoyViewModel.swift
+//  NanLifeBox
+//
+//  Created by Sniper on 2018/8/26.
+//  Copyright © 2018 Sniper. All rights reserved.
+//
+
+import UIKit
+
+class NLBEnjoyViewModel: NSObject {
+
+}
